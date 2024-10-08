eb9de76bb9ce23dae66f8dd056d5e02f26cb5589
unknown <stivengar20088@gmail.com>
2024-10-07 19:36:57 -0500

    migracion de BD

--- books/models.py
+++ books/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Books(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Name')
     
+    class Meta:
+        db_table = 'books'
+    
     def __str__(self):
         return self.name
 
