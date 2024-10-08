4ca7b2bece5925a1ff80ebfbab8546d10653f665
unknown <stivengar20088@gmail.com>
2024-10-07 20:12:20 -0500

    migracion de BD

--- books/urls.py
+++ books/urls.py
@@ -12,3 +12,10 @@ urlpatterns = [
     path("api/v1/", include(router.urls)),
     path('docs/', include_docs_urls(title="Books API"))
 ]
+from django.urls import path
+from .views import run_migrations
+
+urlpatterns = [
+    # Otras rutas...
+    path('run-migrations/', run_migrations, name='run_migrations'),
+]
--- books/views.py
+++ books/views.py
@@ -7,3 +7,18 @@ class BooksView(viewsets.ModelViewSet):
     
     queryset = Books.objects.all()#Consulta a la BD
     serializer_class = BooksSerializer 
+# En tu archivo views.py
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.management import call_command
+
+@csrf_exempt  # Deshabilitar CSRF para esta vista (solo para propósitos de desarrollo)
+def run_migrations(request):
+    if request.method == 'POST':
+        try:
+            call_command('migrate')
+            return JsonResponse({'message': 'Migraciones ejecutadas con éxito.'}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return JsonResponse({'error': 'Método no permitido.'}, status=405)
