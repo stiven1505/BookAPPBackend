69f745889ea67e6fea05b140311eab7b5041971c
Brayan Garcia <83484277+stiven1505@users.noreply.github.com>
2025-03-26 18:33:19 -0500

    Update views.py

--- books/views.py
+++ books/views.py
@@ -8,6 +8,3 @@ class BooksView(viewsets.ModelViewSet):
     queryset = Books.objects.all()#Consulta a la BD
     serializer_class = BooksSerializer 
 
-def check_books_data(request):
-    books = list(Book.objects.values())  # Convierte los registros en un diccionario
-    return JsonResponse({"books": books})
