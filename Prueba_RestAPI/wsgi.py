a3258b1e0e5c2986d29fc463d0714471df9fda1b
Brayan Garcia <83484277+stiven1505@users.noreply.github.com>
2025-03-26 22:17:49 -0500

    Update wsgi.py

--- Prueba_RestAPI/wsgi.py
+++ Prueba_RestAPI/wsgi.py
@@ -13,6 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prueba_RestAPI.settings')
 
+django.setup()
+
 # Ejecutar migraciones automáticamente al iniciar
 from django.core.management import call_command
 call_command("migrate")
