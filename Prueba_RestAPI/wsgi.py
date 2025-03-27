50b7cda05a0e19029ad62594f9ab515b22883af2
Brayan Garcia <83484277+stiven1505@users.noreply.github.com>
2025-03-26 22:32:01 -0500

    Update wsgi.py

--- Prueba_RestAPI/wsgi.py
+++ Prueba_RestAPI/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prueba_RestAPI.settings')
+django.setup()
+call_command('migrate')
 
 application = get_wsgi_application()
