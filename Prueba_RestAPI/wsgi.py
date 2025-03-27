c539456e5ebbca839f7d61c952a50a119c5d9251
Brayan Garcia <83484277+stiven1505@users.noreply.github.com>
2025-03-26 22:36:19 -0500

    Update wsgi.py

--- Prueba_RestAPI/wsgi.py
+++ Prueba_RestAPI/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 
