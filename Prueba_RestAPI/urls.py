d7391c4ffd3b55dbdf1049a3182de4dcd48a210c
Brayan Garcia <83484277+stiven1505@users.noreply.github.com>
2025-03-26 22:34:17 -0500

    Update urls.py

--- Prueba_RestAPI/urls.py
+++ Prueba_RestAPI/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.core.management import call_command
+from django.http import JsonResponse
+
+def run_migrations(request):
+    call_command('migrate')
+    return JsonResponse({"message": "Migraciones ejecutadas con éxito"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',include('books.urls'))
+    path('books/',include('books.urls')),
+    path('run-migrations/', run_migrations, name="run-migrations")
 ]
